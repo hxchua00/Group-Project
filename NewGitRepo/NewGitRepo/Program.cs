f7da7ab4601da899cdb1072f491b24e987674d8b
hxchua00 <hxchua00@gmail.com>
2021-09-10 17:29:30 +0800

    2nd commit

--- NewGitRepo/NewGitRepo/Program.cs
+++ NewGitRepo/NewGitRepo/Program.cs
@@ -5,12 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NewGitRepo
-{
+{ 
+    class Item
+    {
+        string name;
+        double Weight;
+        double Price;
+        public double GetPrice() 
+        {
+            return Price;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+
+    }
+
+    class Bill
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there! Welcome!");
+            //Add a number of items
+            //Call GetFinalBill()
+            //GetTotalWeight()
+
+            Console.WriteLine("");
             Console.ReadLine();
         }
     }
